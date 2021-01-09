9b97a45b5222e77630371bd0619735a6ab2c732b
Brighton Balfrey <balfrey@usc.edu>
2021-01-08 18:30:25 -0800

    added some comments, fixed simple example

--- examples/simple.c
+++ examples/simple.c
@@ -21,24 +21,38 @@ char* json_readfile(char *path)
     }
 
     res[i] = '\0';
-
+    fclose(file);
     return res;
 }
 
 int main()
 {
-    char *reddit = json_readfile("/files/examples/javascript_reddit.json");
+    char *reddit = json_readfile("./javascript_reddit.json");
     json_parser *p = json_parser_create(reddit);
     bool res = json_parseobj(p, p->all_tokens->tokens[0]);
-    // Get first child of outer token object (which should be outer json object)
+
+
+    /* Get first child of outer token object (which should be outer json object */
     json_jsontoken *obj = p->all_tokens->tokens[0]->children->tokens[0];
-    // Print available keys in first level
+    printf("%d\n", obj->children->length);
+
+    /* Print available keys in first level */
     for (int i = 0; i < obj->children->length; i++) {
-        printf("key %d: ", i+1);
-        for (int j = obj->children->tokens[i]->start_in;
-            j < obj->children->tokens[i]->end_in; j++) printf("%c", p->input[j]);
-        printf("%n");
+        printf("key %d: ", i);
+        for (int j = obj->children->tokens[i]->start_in; j < obj->children->tokens[i]->end_in; j++) {
+            printf("%c", p->input[j]);
+        }
+        printf("\n");
     }
+    /*
+        Notice that obj has two children: `kind` and `data`. And since objs' type is `JSON_OBJ`, we
+        know that it's children are keys. Each key will have children of its own. Print out the child
+        of the data key.
+     */
+    json_jsontoken* datkey = obj->children->tokens[1]->children->tokens[0];
+    for (int j = datkey->start_in; j < datkey->end_in; j++)
+        printf("%c", p->input[j]);
+    printf("\n");
     json_parser_cleanup(p);
     free(reddit);
 }
